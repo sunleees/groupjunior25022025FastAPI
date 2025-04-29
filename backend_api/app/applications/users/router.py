7f14c9d803ff1beb305e1d63432ab1f033cdb295
sunleees <koyfplaton@gmail.com>
2025-04-29 20:58:58 +0300

    router

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, status
+from app.applications.users.shemas import RegisterUserFields, BaseFields
+from app.settings import settings
 
 router_users = APIRouter()
 
 
-@router_users.get("/")
-async def index():
-    return []
+@router_users.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_user(new_user: RegisterUserFields) -> BaseFields:
+    print(settings.POSTGRES_DB, 6666666666666666666666)
+    return new_user
