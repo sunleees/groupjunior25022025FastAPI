aa2cbce7dfc1885d50d4f4a42b7ef18b3d450c4d
sunleees <koyfplaton@gmail.com>
2025-05-16 19:26:17 +0300

    5-hash password

--- backend_api/app/app_factory.py
+++ backend_api/app/app_factory.py
@@ -8,4 +8,5 @@ def get_application() -> FastAPI:
     app = FastAPI(root_path="/api", root_path_in_servers=True, debug=settings.DEBUG)
     app.include_router(router_users, prefix="/users", tags=["Users"])
 
+
     return app
