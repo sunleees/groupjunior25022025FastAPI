8e2f2b6c75242eef5d975782b743153354e1f8b1
sunleees <koyfplaton@gmail.com>
2025-05-16 19:02:18 +0300

    5-hash password

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,20 +1,28 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from applications.database.session_dependencies import get_async_session
+from applications.auth.password_handler import PasswordEncrypt
 from applications.users.models import User
 from applications.users.shemas import BaseFields, RegisterUserFields
+from applications.database.session_dependencies import get_async_session
 
 router_users = APIRouter()
 
 
 async def create_user_in_db(email, name, password, session: AsyncSession):
-    new_user = User(email=email, hashed_password=password, name=name)
+    hashed_password = await PasswordEncrypt.get_password_hash(password)
+    new_user = User(
+        email=email,
+        hashed_password=hashed_password,
+        name=name,
+    )
     session.add(new_user)
     await session.commit()
 
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_user(new_user: RegisterUserFields, session: AsyncSession = Depends(get_async_session)) -> BaseFields:
+async def create_user(
+    new_user: RegisterUserFields, session: AsyncSession = Depends(get_async_session)
+) -> BaseFields:
     await create_user_in_db(new_user.email, new_user.name, new_user.password, session)
     return new_user
