55e4324b8de780f5efef3be5ed1247e3e1bc2ba5
sunleees <koyfplaton@gmail.com>
2025-05-27 20:44:39 +0300

    setup login_comp.html

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -8,6 +8,13 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/")
 async def index(request: Request):
-    context = {"request": request, "data": 123}
+    context = {"request": request}
     response = templates.TemplateResponse("index.html", context=context)
+    return response
+
+
+@router.get("/login")
+async def login(request: Request):
+    context = {"request": request}
+    response = templates.TemplateResponse("login.html", context=context)
     return response
