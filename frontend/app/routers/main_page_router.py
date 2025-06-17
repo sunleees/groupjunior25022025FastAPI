27c716d56493256e7814a8db654bc8d5f650c837
sunleees <koyfplaton@gmail.com>
2025-06-17 19:26:21 +0300

    create user web

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -1,20 +1,12 @@
 from fastapi import APIRouter, Request, Form, Depends, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
-import httpx
+
+from backend_api.api import get_current_user_with_token, login_user, register_user
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory="templates")
-
-
-async def get_current_user_with_token(request: Request) -> dict:
-    access_token = request.cookies.get("access_token")
-    if not access_token:
-        return {}
-    user = await get_user_info(access_token)
-    user["access_token"] = access_token
-    return user
 
 
 @router.get("/")
@@ -26,26 +18,6 @@ async def index(request: Request, user: dict = Depends(get_current_user_with_tok
     return response
 
 
-async def login_user(user_email: str, password: str):
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            url="http://backend_api:9999/api/auth/login",
-            data={"username": user_email, "password": password},
-        )
-        print(response.json())
-        return response.json()
-
-
-async def get_user_info(access_token: str):
-    async with httpx.AsyncClient() as client:
-        response = await client.get(
-            url="http://backend_api:9999/api/auth/get-my-info",
-            headers={"Authorization": f"Bearer {access_token}"},
-        )
-        print(response.json())
-        return response.json()
-
-
 @router.get("/login")
 @router.post("/login")
 async def login(
@@ -55,7 +27,6 @@ async def login(
     password: str = Form(""),
 ):
     context = {"request": request, "entered_email": user_email}
-    print(user, 88888888888888888888888888)
     redirect_url = request.url_for("index")
     if user.get("name"):
         response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
@@ -72,8 +43,6 @@ async def login(
         errors = ["Incorrect login or password"]
         context["errors"] = errors
         return templates.TemplateResponse("login.html", context=context)
-    print(6666666666666666666666666)
-
     response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
     response.set_cookie(
         key="access_token", value=access_token, httponly=True, max_age=60 * 5
@@ -86,4 +55,46 @@ async def logout(request: Request):
     redirect_url = request.url_for("login")
     response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
     response.delete_cookie("access_token")
+    return response
+
+
+@router.get("/register")
+@router.post("/register")
+async def register(
+    request: Request,
+    user: dict = Depends(get_current_user_with_token),
+    user_email: str = Form(""),
+    password: str = Form(""),
+    user_name: str = Form(""),
+):
+    context = {
+        "request": request,
+        "entered_email": user_email,
+        "entered_name": user_name,
+    }
+    redirect_url = request.url_for("index")
+    if user.get("name"):
+        response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+        return response
+
+    if request.method == "GET":
+        response = templates.TemplateResponse("register.html", context=context)
+        response.delete_cookie("access_token")
+        return response
+
+    created_user = await register_user(
+        user_email=user_email, password=password, name=user_name
+    )
+    if created_user.get("email"):
+        user_tokens = await login_user(user_email, password)
+        access_token = user_tokens.get("access_token")
+
+        response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+        response.set_cookie(
+            key="access_token", value=access_token, httponly=True, max_age=60 * 5
+        )
+        return response
+
+    context["errors"] = [created_user["detail"]]
+    response = templates.TemplateResponse("register.html", context=context)
     return response
