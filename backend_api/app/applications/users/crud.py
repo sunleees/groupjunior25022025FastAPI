fa770a71c8ccf13df8c11ab2f3ee001b305b4a0a
sunleees <koyfplaton@gmail.com>
2025-06-17 21:03:03 +0300

    save user

--- backend_api/app/applications/users/crud.py
+++ backend_api/app/applications/users/crud.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
@@ -22,4 +24,8 @@ async def get_user_by_email(email, session: AsyncSession) -> User | None:
 
 
 async def activate_user_account(user_uuid, session: AsyncSession):
-    pass
+    query = select(User).filter(User.uuid_data == user_uuid)
+    result = await session.execute(query)
+    user = result.scalar_one_or_none()
+    if not user:
+        raise HTTPException(status_code=400, detail="Provided data does not belongs ")
--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,8 +1,6 @@
 import uuid
-
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from applications.database.session_dependencies import get_async_session
 from applications.users.crud import (
     create_user_in_db,
