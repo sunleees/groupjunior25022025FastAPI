f3b1c60cd8dda2ef8fb5160a13bbce8548fe0a63
sunleees <koyfplaton@gmail.com>
2025-07-03 14:22:46 +0300

    extract products 3-4-5

--- backend_api/app/applications/products/crud.py
+++ backend_api/app/applications/products/crud.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import asc, desc, select, func
+from sqlalchemy import asc, desc, select, func, or_, and_
 import math
 
 from applications.products.models import Product
@@ -22,8 +22,8 @@ async def create_product_in_db(
     """
     new_product = Product(
         uuid_data=product_uuid,
-        title=title,
-        description=description,
+        title=title.strip(),
+        description=description.strip(),
         price=price,
         main_image=main_image,
         images=images,
@@ -39,8 +39,25 @@ async def get_products_data(params: SearchParamsSchema, session: AsyncSession):
     count_query = select(func.count()).select_from(Product)
 
     order_direction = asc if params.order_direction == SortEnum.ASC else desc
+    if params.q:
+        search_fields = [Product.title, Product.description]
+        if params.use_sharp_q_filter:
+            cleaned_query = params.q.strip().lower()
+            search_condition = [
+                func.lower(search_field) == cleaned_query
+                for search_field in search_fields
+            ]
+            query = query.filter(or_(*search_condition))
+            count_query = count_query.filter(or_(*search_condition))
+        else:
+            words = [word for word in params.q.strip().split() if len(word) > 1]
+            search_condition = or_(
+                and_(*(search_field.icontains(word) for word in words))
+                for search_field in search_fields
+            )
+            query = query.filter(search_condition)
+            count_query = count_query.filter(search_condition)
 
-    # if params.q:
     sort_field = Product.price if params.sort_by == SortByEnum.PRICE else Product.id
     query = query.order_by(order_direction(sort_field))
     offset = (params.page - 1) * params.limit
