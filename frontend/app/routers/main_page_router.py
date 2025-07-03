ca177d704df64195d94504fdee1a2ba80b9b7a42
sunleees <koyfplaton@gmail.com>
2025-07-03 18:20:25 +0300

    extract products front 3

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -17,7 +17,7 @@ templates = Jinja2Templates(directory="templates")
 @router.get("/")
 async def index(request: Request, user: dict = Depends(get_current_user_with_token)):
     products = await get_products()
-    context = {"request": request, "products": products}
+    context = {"request": request, "products": products["items"]}
     print(products, 555555555555555555)
     if user.get("name"):
         context["user"] = user
